cce8e5dcb3a0b22b717225b356fb98cf7d84100b
MrSupertash <mrsupertash@gmail.com>
2023-01-26 11:25:19 +0100

    refactored function fixes #5

--- JavaScript/CodeWars/8kyu/2023-01-26/simpleMultiplication.js
+++ JavaScript/CodeWars/8kyu/2023-01-26/simpleMultiplication.js
@@ -2,10 +2,4 @@
 
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-// function simpleMultiplication(number) {
-//     if (number % 2 === 0) {
-//         return number * 8;
-//     } else {
-//         return number * 9;
-//     }
-// }
+const simpleMultiplication = num => num % 2 === 0 ? num * 8 : num * 9;
