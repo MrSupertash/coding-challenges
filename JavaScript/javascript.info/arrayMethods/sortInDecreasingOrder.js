9e7406bdefba586abd42cac18ac83ec5e2dea91a
MrSupertash <mrsupertash@gmail.com>
2023-02-03 13:36:07 +0100

    added sorting exercise

---
+++ JavaScript/javascript.info/arrayMethods/sortInDecreasingOrder.js
@@ -0,0 +1,14 @@
+// Description
+
+// Sort in decreasing order
+
+let arr = [5, 2, 1, -10, 8];
+
+// // ... your code to sort it in decreasing order
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+
+// Solution
+
+console.log(arr.sort((a, b) => b - a));
