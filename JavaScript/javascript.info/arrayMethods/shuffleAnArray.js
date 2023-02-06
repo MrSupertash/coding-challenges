b8785ef112fceca835fc8820e46669243461fb00
MrSupertash <mrsupertash@gmail.com>
2023-02-06 12:28:16 +0100

    Fisher-Yates shuffle. Not having a clear head right now, though. Going to watch The Last of Us (HBO) episode 4 from last night instead.

--- JavaScript/javascript.info/arrayMethods/shuffleAnArray.js
+++ JavaScript/javascript.info/arrayMethods/shuffleAnArray.js
@@ -7,7 +7,7 @@
 
 // Multiple runs of shuffle may lead to different orders of elements. For instance:
 
-let arr = [1, 2, 3];
+// let arr = [1, 2, 3];
 
 // shuffle(arr);
 // // arr = [3, 2, 1]
@@ -25,15 +25,30 @@ let arr = [1, 2, 3];
 
 // This was my first attempt but I knew whatever would happen here, that it might not have equal probability. However, it doesn't work at all and I have yet to wrap my head around what actuall happens here. Taking a break now.
 
-function shuffle(arr) {
+// function shuffle(arr) {
     
 
-    for (let i = 0; i < arr.length; i++) {
-        let rand = Math.floor(Math.random() * arr.length);
-        let x = i;
-        arr.splice(i, 1, rand);
-        arr.splice(rand, 1, x);
+//     for (let i = 0; i < arr.length; i++) {
+//         let rand = Math.floor(Math.random() * arr.length);
+//         let x = i;
+//         arr.splice(i, 1, rand);
+//         arr.splice(rand, 1, x);
+//     }
+// }
+
+
+// 2nd attempt - I saw the solution before using Fisher-Yates, let's try to recreate. The idea is to swap the last index of the array with a random one before it and iterate over it backwards
+
+let arr = [1, 2, 3];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let x = array[i];
+        array[i] = array[j];
+        array[j] = x;
     }
 }
 
 shuffle(arr);
+console.log(arr);
